15a6a1535935de67fed0c63da21be15947866360
Abhi <abhi@Abhis-MacBook-Air.local>
2026-09-06 15:16:59 +0530

    spring init code and docker-compose file

---
+++ src/main/java/com/example/ProductImageStudio/ProductImageStudioApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProductImageStudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductImageStudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductImageStudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProductImageStudio/ProductImageStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProductImageStudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductImageStudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
