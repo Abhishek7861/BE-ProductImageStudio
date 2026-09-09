19b9c6623ac374e7cd78115ecd1003d0537b9755
Abhi <abhi@Abhis-MacBook-Air.local>
2026-09-09 15:11:56 +0530

    removed redundant bean

--- src/main/java/com/example/ProductImageStudio/config/GcpStorageConfig.java
+++ src/main/java/com/example/ProductImageStudio/config/GcpStorageConfig.java
@@ -7,8 +7,8 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GcpStorageConfig {
-    @Bean
-    public Storage storage() {
-        return StorageOptions.getDefaultInstance().getService();
-    }
+//    @Bean
+//    public Storage storage() {
+//        return StorageOptions.getDefaultInstance().getService();
+//    }
 }
